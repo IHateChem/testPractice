38fc5e7717f0634c140c660cdce376755b46427f
LEE <syleelsw@gmail.com>
2022-06-20 09:14:22 +0900

    start

--- source/repos/aa.cpp
+++ source/repos/aa.cpp
@@ -1,23 +1,13 @@
 #include <stdio.h>
+#include <cstring>
 
 int main() {
 
-    int a;
+    char a[] = scanf();
     int b;
-    scanf("%d", &a);
     scanf("%d", &b);
     int partition;
-    if (a < 0) {
-        partition = 2;
-    }
-    else {
-        partition = 1;
-    }
-    if (b < 0) {
-        partition = partition + 4;
-    }
-    partitiion = partition % 4
-        printf();
+    print(a)
     return 0;
 
 }
